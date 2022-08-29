93c31be1a39e8615701d73b224b699f4e25d7e71
Joey Meisner <emptygrocerybag@hotmail.com>
2022-08-29 15:25:56 +0000

    Rudimentary paging

--- src/c/kernel.c
+++ src/c/kernel.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdint.h>
 
 #include "tty.h"
@@ -6,9 +7,23 @@
 int fg_color = WHITE;
 int bg_color = BLACK;
 
+extern void enable_paging(uint32_t* dir);
+
 void kernel_main(uint16_t* term) {
   //extern uint32_t* _HEAP;
-
   puts("kernel_main()\n", term);
-  puts("Hello, World!\n", term);
+
+  uint32_t page_directory[1024] __attribute__((aligned(4096)));
+  for (int i = 0; i < 1024; i++)
+    page_directory[i] = 2;
+
+  // Identity map first 1MB of RAM
+  uint32_t page_table[1024] __attribute__((aligned(4096)));
+  for (int i = 0; i < 1023; i++) {
+    page_table[i] = (i * 0x1000) | 3;
+  }
+
+  page_directory[0] = ((unsigned int)page_table) | 3;
+  puts("enabling paging...\n", term);
+  enable_paging(page_directory);
 }
